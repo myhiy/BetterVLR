d82d7019352956fc072a1a8358b714422df1143c
myhiy <myhiyyyy@gmail.com>
2023-07-19 21:41:05 +0100

    use 7TV api for emotes, cleanup emoji picker code

--- src/emojis.js
+++ src/emojis.js
@@ -52,17 +52,15 @@ function parseTwemojis(selector) {
 
 // Convert custom emoji shortcodes to emoji images
 function parseCustomEmojis(selector) {
-  $.getJSON("https://snippet.host/sgyddt/raw", function (data) {
+  $.getJSON("https://7tv.io/v3/emote-sets/64aa3859c7d082e76f7248c8", function (data) {
     $(selector).each(function () {
       var post_body = $(this);
       var html_content = post_body.html();
 
-      data.forEach(function (category) {
-        category.emojis.forEach(function (emoji) {
-          var regex = new RegExp(":" + emoji.id + ":", "g");
-          html_content = html_content.replace(regex, `
-          <img class="custom-emoji" draggable="false" src="${emoji.skins[0].src}" alt=":${emoji.id}:" title=":${emoji.id}:">`);
-        });
+      data.emotes.forEach(function (emoji) {
+        var regex = new RegExp(":" + emoji.name + ":", "g");
+        html_content = html_content.replace(regex, `
+        <img class="custom-emoji" draggable="false" src="${emoji.data.host.url}/1x.webp" alt=":${emoji.name}:" title=":${emoji.name}:">`);
       });
 
       post_body.html(html_content);
@@ -99,54 +97,71 @@ function getTheme() {
 }
 
 // Add emoji picker button
-$(".post-editor-header-action[title=Spoiler]").after(`<li class="post-editor-header-action emoji-button" title="Emoji Picker"><i class="fa fa-smile-o"></i></li>`);
+const emoji_picker_button = `<li class="post-editor-header-action emoji-button" title="Emoji Picker"><i class="fa fa-smile-o"></i></li>`;
+$(".post-editor-header-action[title=Spoiler]").after(emoji_picker_button);
 
-$.getJSON("https://snippet.host/sgyddt/raw", function (custom) {
+$.getJSON("https://7tv.io/v3/emote-sets/64aa3859c7d082e76f7248c8", function (custom) {
+  // Create the category object for custom emojis
+  const custom_emojis = custom.emotes.map((emoji) => ({
+    id: emoji.name,
+    name: emoji.name,
+    keywords: emoji.data.tags,
+    skins: [{ src: `${emoji.data.host.url}/1x.webp` }],
+  }));
+
+  const custom_category = {
+    id: "custom",
+    name: "Custom",
+    emojis: custom_emojis,
+  };
+
+  // Wrap the custom_category object in an array
+  const custom_category_array = [custom_category];
+
   const picker_options = {
     set: "twitter",
     skinTonePosition: "search",
     theme: getTheme(),
-    custom: custom,
-    categories: ["frequent", "custom", "teams", "agents", "people", "nature", "foods", "activity", "places", "objects", "symbols", "flags"],
+    custom: custom_category_array,
+    categories: ["frequent", "custom", "people", "nature", "foods", "activity", "places", "objects", "symbols", "flags"],
     onEmojiSelect: addEmoji,
     onClickOutside: closePicker,
     onAddCustomEmoji: requestEmoji
   }
   const picker = new Picker(picker_options);
-  $(picker).appendTo("body").attr("id", "popup");
-
-  const emoji_button = $(".emoji-button")[1];
-  const popup = $("#popup")[0];
+  $("body").append(picker);
 
   let popper = null;
 
   let last_focused_textarea = null;
 
-  // Handle textarea focus event
+  // Handle post-editor focus event
   $(document).on("focus", ".post-editor-text", function () {
     last_focused_textarea = $(this);
   });
 
-  $(document).on("click", ".emoji-button", togglePopup);
+  $(document).on("click", ".emoji-button", togglePicker);
 
-  function togglePopup() {
+  function togglePicker() {
     if (popper) {
       popper.destroy();
       popper = null;
-      $("#popup").hide();
+
+      $(picker).hide();
     } else {
-      popper = createPopper(this, popup, {
+      popper = createPopper(this, picker, {
         placement: "top",
         modifiers: [{ name: "offset", options: { offset: [0, 8], } }]
       });
-      $("#popup").show();
-      const popup_input = popup.shadowRoot.querySelector("input");
-      popup_input.focus({ preventScroll: true });
+
+      $(picker).show();
+
+      const picker_input = picker.shadowRoot.querySelector("input");
+      picker_input.focus({ preventScroll: true });
     }
   }
 
-
-  // Insert emoji into textarea on select
+  // Insert emoji into post-editor on select
   function addEmoji(emoji, event) {
     if (last_focused_textarea) {
       const textarea = last_focused_textarea;
@@ -164,9 +179,9 @@ $.getJSON("https://snippet.host/sgyddt/raw", function (custom) {
 
     popper.destroy();
     popper = null;
-    $("#popup").hide();
-  }
 
+    $(picker).hide();
+  }
 
   // Hide picker when clicking outside of it
   function closePicker(event) {
@@ -175,14 +190,14 @@ $.getJSON("https://snippet.host/sgyddt/raw", function (custom) {
     if (popper && !is_emoji_button) {
       popper.destroy();
       popper = null;
-      $("#popup").hide();
+
+      $(picker).hide();
     }
   }
 
-
-  // Request custom emoji
+  // Request custom emojis
   function requestEmoji() {
-    togglePopup();
+    togglePicker();
     $.magnificPopup.open({
       items: {
         src: `
