ffa8212f66056772e3f6c8c75f667ca9e2b1dd36
myhiy <myhiyyyy@gmail.com>
2023-09-08 18:33:34 +0100

    zip only on build

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,14 +1,7 @@
 const { merge } = require("webpack-merge");
 const config = require("./webpack.config.js");
-const ZipPlugin = require("zip-webpack-plugin");
 
 module.exports = merge(config, {
   mode: "development",
   devtool: "inline-source-map",
-  plugins: [
-    new ZipPlugin({
-      path: "../",
-      filename: "bettervlr.zip",
-    })
-  ]
-})
+});
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,15 @@
 const { merge } = require("webpack-merge");
 const config = require("./webpack.config.js");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const ZipPlugin = require("zip-webpack-plugin");
 
 module.exports = merge(config, {
   mode: "production",
   plugins: [
-    new CssMinimizerPlugin()
-  ]
-})
+    new CssMinimizerPlugin(),
+    new ZipPlugin({
+      path: "../",
+      filename: "bettervlr.zip",
+    }),
+  ],
+});
