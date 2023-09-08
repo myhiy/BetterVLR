1d3174ea24fcfa414c9c6918fbd581eb9801f6db
myhiy <myhiyyyy@gmail.com>
2023-09-08 18:24:58 +0100

    zip on build

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,14 @@
 const { merge } = require("webpack-merge");
 const config = require("./webpack.config.js");
+const ZipPlugin = require("zip-webpack-plugin");
 
 module.exports = merge(config, {
   mode: "development",
-  devtool: "inline-source-map"
+  devtool: "inline-source-map",
+  plugins: [
+    new ZipPlugin({
+      path: "../",
+      filename: "bettervlr.zip",
+    })
+  ]
 })
